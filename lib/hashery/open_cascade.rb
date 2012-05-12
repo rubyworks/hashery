6d935d6012dc984c52f838d12688c1d7a73a5ff0
7rans <transfire@gmail.com>
2012-05-12 16:01:05 -0400

    Put OpenCascade into Hashery namespace.

--- lib/hashery/open_cascade.rb
+++ lib/hashery/open_cascade.rb
@@ -1,87 +1,109 @@
 require 'hashery/open_hash'
 #require 'facets/nullclass'
 
-# OpenCascade is subclass of OpenHash. It differs in a few
-# significant ways.
-#
-# The main reason this class is labeled "cascade", every internal
-# Hash is transformed into an OpenCascade dynamically upon access.
-# This makes it easy to create "cascading" references.
-#
-#   h = { :x => { :y => { :z => 1 } } }
-#   c = OpenCascade[h]
-#   c.x.y.z  #=> 1
-#
-# As soon as you access a node it automatically becomes an OpenCascade.
-#
-#   c = OpenCascade.new   #=> #<OpenCascade:0x7fac3680ccf0 {}>
-#   c.r                   #=> #<OpenCascade:0x7fac368084c0 {}>
-#   c.a.b                 #=> #<OpenCascade:0x7fac3680a4f0 {}>
-#
-# But if you set a node, then that will be it's value.
-#
-#   c.a.b = 4             #=> 4
-#
-# To query a node without causing the auto-creation of an OpenCasade
-# object, use the ?-mark.
-#
-#   c.a.z?                #=> nil
-#
-# OpenCascade also transforms Hashes within Arrays.
-#
-#  h = { :x=>[ {:a=>1}, {:a=>2} ], :y=>1 }
-#  c = OpenCascade[h]
-#  c.x.first.a.assert == 1
-#  c.x.last.a.assert == 2
-#
-# Finally, you can set a node and get the reciever back using
-# the !-mark.
-#
-#   c = OpenCascade.new   #=> #<OpenCascade:0x7fac3680ccf0 {}>
-#   c.x!(4).y!(3)         #=> #<OpenCascade:0x7fac3680ccf0 {:x=>4, :y=>3}>
-#
-class OpenCascade < OpenHash
+module Hashery
 
+  # OpenCascade is subclass of OpenHash. It differs in a few
+  # significant ways.
   #
-  def method_missing(sym, *args, &blk)
-    type = sym.to_s[-1,1]
-    name = sym.to_s.gsub(/[=!?]$/, '').to_sym
-    case type
-    when '='
-      self[name] = args.first
-    when '!'
-      #@hash.__send__(name, *args, &blk)
-      __send__(name, *args, &blk)
-    when '?'
-      self[name]
-    else
-      if key?(name)
-        self[name] = transform_entry(self[name])
+  # The main reason this class is labeled "cascade", every internal
+  # Hash is transformed into an OpenCascade dynamically upon access.
+  # This makes it easy to create "cascading" references.
+  #
+  #   h = { :x => { :y => { :z => 1 } } }
+  #   c = OpenCascade[h]
+  #   c.x.y.z  #=> 1
+  #
+  # As soon as you access a node it automatically becomes an OpenCascade.
+  #
+  #   c = OpenCascade.new   #=> #<OpenCascade:0x7fac3680ccf0 {}>
+  #   c.r                   #=> #<OpenCascade:0x7fac368084c0 {}>
+  #   c.a.b                 #=> #<OpenCascade:0x7fac3680a4f0 {}>
+  #
+  # But if you set a node, then that will be it's value.
+  #
+  #   c.a.b = 4             #=> 4
+  #
+  # To query a node without causing the auto-creation of an OpenCasade
+  # object, use the ?-mark.
+  #
+  #   c.a.z?                #=> nil
+  #
+  # OpenCascade also transforms Hashes within Arrays.
+  #
+  #   h = { :x=>[ {:a=>1}, {:a=>2} ], :y=>1 }
+  #   c = OpenCascade[h]
+  #   c.x.first.a.assert == 1
+  #   c.x.last.a.assert == 2
+  #
+  # Finally, you can set a node and get the reciever back using
+  # the !-mark.
+  #
+  #   c = OpenCascade.new   #=> #<OpenCascade:0x7fac3680ccf0 {}>
+  #   c.x!(4).y!(3)         #=> #<OpenCascade:0x7fac3680ccf0 {:x=>4, :y=>3}>
+  #
+  class OpenCascade < OpenHash
+
+    #
+    def self.[](hash)
+      oc = new
+      hash.each{ |(k,v)| oc.store(k,v) }
+      oc
+    end
+
+    #
+    def initialize(*a)
+      super(*a){ |h,k| h[k] = OpenCascade.new }
+    end
+
+    #
+    def store(key, value)
+      super(key, cast_value(value))
+    end
+
+    #
+    def method_missing(sym, *args, &blk)
+      type = sym.to_s[-1,1]
+      name = sym.to_s.gsub(/[=!?]$/, '').to_sym
+      case type
+      when '='
+        store(name, args.first)
+      when '!'
+        #@hash.__send__(name, *args, &blk)
+        __send__(name, *args, &blk)
+      when '?'
+        key?(name) ? self[name] : nil
+        #key?(name)
       else
-        self[name] = OpenCascade.new #self.class.new
+        if key?(name)
+          self[name] #= transform_entry(self[name])
+        else
+          self[name] #= OpenCascade.new #self.class.new
+        end
       end
     end
-  end
 
-  def each
-    super do |key, entry|
-      yield([key, transform_entry(entry)])
-    end
-  end
+    #def each
+    #  super do |key, entry|
+    #    yield([key, transform_entry(entry)])
+    #  end
+    #end
 
   private
 
     #
-    def transform_entry(entry)
+    def cast_value(entry)
       case entry
       when Hash
-        OpenCascade.new(entry) #self.class.new(val)
+        OpenCascade[entry] #self.class.new(val)
       when Array
-        entry.map{ |e| transform_entry(e) }
+        entry.map{ |e| cast_value(e) }
       else
         entry
       end
     end
+
+  end
 
 end
 
