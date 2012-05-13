ba9bbc36473165d4a4d19704de890b893ce67299
7rans <transfire@gmail.com>
2012-05-13 13:42:56 -0400

    Setup coverage configuration. [test]

---
+++ Config.rb
@@ -0,0 +1,27 @@
+config 'rubytest' do
+  $:.unshift 'test'
+  $:.unshift 'lib'
+end
+
+profile 'coverage' do
+  config 'rubytest' do
+    $:.unshift 'test'
+    $:.unshift 'lib'
+
+    require 'simplecov'
+    SimpleCov.command_name 'RubyTest'
+    SimpleCov.start do
+      add_filter '/test/'
+      coverage_dir 'log/coverage'
+    end
+  end
+
+  config 'qed' do
+    require 'simplecov'
+    SimpleCov.command_name 'QED'
+    SimpleCov.start do
+      add_filter '/demo/'
+      coverage_dir 'log/coverage'
+    end
+  end
+end
